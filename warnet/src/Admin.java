5062116888157b0945b2f35d22db9531beac729d
Bagus Akmal <101700139+bagusakmal@users.noreply.github.com>
2022-11-04 06:51:23 +0700

    Add files via upload

---
+++ warnet/src/Admin.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class Admin {
+    private String uesrname;
+    private String password;
+    User u = new User();
+    Transaksi t = new Transaksi();
+    Member m = new Member();
+    Komputer k = new Komputer();
+    Scanner in=new Scanner(System.in);
+    protected void melihat(int a, int b){
+        System.out.println("Id user : "+a);
+        System.out.println("biaya : "+b);
+    }
+    private String user(){
+        String u = "admin";
+        return u;
+    }
+    private String pass(){
+        String p = "admin";
+        return p;
+    }
+    protected boolean mengisi(){
+        boolean a = false;
+        Scanner in=new Scanner(System.in);
+        System.out.print("Masukkan username : ");
+        uesrname = in.next();
+        setUesrname(uesrname);
+
+
+
+        if(uesrname.equals(user())){
+            System.out.print("Masukkan Password : ");
+            password=in.next();
+            setPassword(password);
+            if(password.equals(pass())){
+                a = true;
+            }
+            else{
+                System.out.println("Password Salah");
+            }
+        }
+        else {
+            System.out.println("Bukan Admin");
+        }
+        return a;
+    }
+    protected int mengatur(){
+        int harga;
+        int diskon;
+        System.out.println("Masukkan Tarif harga perjam :");
+        harga = in.nextInt();
+        t.setTarif(harga);
+        return t.setTarif(harga);
+    }
+    protected int mengatur(int a){
+        int diskon;
+        System.out.println("Masukkan Diskon untuk member(%) : ");
+        diskon = in.nextInt();
+        t.setDiskon(a*diskon/100);
+        return t.setDiskon(a*diskon/100);
+    }
+    private void menghapus(){
+
+    }
+
+    public String getUesrname() {
+        return uesrname;
+    }
+
+    public void setUesrname(String uesrname) {
+        this.uesrname = uesrname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ warnet/src/Komputer.java
@@ -0,0 +1,24 @@
+public class Komputer {
+    int noKomputer;
+    int harga;
+
+    public int hitung(int a){
+        return a;
+    }
+
+    public int getNoKomputer() {
+        return noKomputer;
+    }
+
+    public void setNoKomputer(int noKomputer) {
+        this.noKomputer = noKomputer;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
